5f53b6a444df7c174e83d2d737ef6e45f0c05bff
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-11 10:16:51 +0000

    updated to extend mase template in first and second view

--- first_app/static/js/script.js
+++ first_app/static/js/script.js
@@ -1,1 +1,41 @@
 console.log('testing testing testing');
+const apiKey = 'HMEY6vWRioll4rhP5rBHRcAKCXvsUEti';
+
+// Create a map style object using the ZXY service.
+const style = {
+    "version": 8,
+    "sources": {
+        "raster-tiles": {
+            "type": "raster",
+            "tiles": [ "https://api.os.uk/maps/raster/v1/zxy/Outdoor_3857/{z}/{x}/{y}.png?key=" + apiKey ],
+            "tileSize": 256
+        }
+    },
+    "layers": [{
+        "id": "os-maps-zxy",
+        "type": "raster",
+        "source": "raster-tiles"
+    }]
+};
+
+// Initialize the map object.
+const map = new maplibregl.Map({
+    container: 'map',
+    minZoom: 6,
+    maxZoom: 19,
+    style: style,
+    maxBounds: [
+        [ -10.76418, 49.528423 ],
+        [ 1.9134116, 61.331151 ]
+    ],
+    center: [ -2.968, 54.425 ],
+    zoom: 13
+});
+
+map.dragRotate.disable(); // Disable map rotation using right click + drag.
+map.touchZoomRotate.disableRotation(); // Disable map rotation using touch rotation gesture.
+
+// Add navigation control (excluding compass button) to the map.
+map.addControl(new maplibregl.NavigationControl({
+    showCompass: false
+}));
--- test_project/settings.py
+++ test_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'test_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR), 'first_app/templates'],
+        'DIRS': [os.path.join(BASE_DIR), 'test_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
